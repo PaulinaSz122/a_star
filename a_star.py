b3b2260b6b7f2cf1cea62d36291306b1a24be1e4
Paulina <paulina.szczepanska122@gmail.com>
2018-11-03 08:46:32 +0100

    Does anyone know how to color the numbers?

--- a_star.py
+++ a_star.py
@@ -33,16 +33,16 @@ def dodaj_o(lista, x, y, l_krokow, h, rodzic_x, rodzic_y):
 
 
 def dodaj_z(lista_zamknieta, pole_o, lista_otwarta):
-    lista_zamknieta[pole_o.x][pole_o.y] = pole_o
+    lista_zamknieta[pole_o.y][pole_o.x] = pole_o
     lista_otwarta.delete(pole_o)
 
 
 def stworz_liste_z(x, y):
     """Utworzenie tablicy do wpisywania wartości listy zamkniętej"""
     lista = []
-    for i in range(x):
+    for i in range(y):
         lista.append([])
-        for j in range(y):
+        for j in range(x):
             lista[i].append(None)
     return lista
 
@@ -74,14 +74,14 @@ def przeszukaj(lista, pole):
 def sprawdz_pole(pole, kierunek, lista_zamknieta, lista_otwarta, mapa):
     x = pole.x + kierunek[0]
     y = pole.y + kierunek[1]
-    if 0 <= x < X and 0 <= y < Y and (x != pole.x or y != pole.y) and mapa[x][y] != "5":
+    if 0 <= x < X and 0 <= y < Y and not (x == pole.rodzic_x and y == pole.rodzic_y) and mapa[y][x] != "5":
         l_krokow = pole.l_krokow + 1
         h = math.sqrt(pow(x - C_X, 2) + pow(y - C_Y, 2)) + l_krokow
         nowe = Pole(x, y, l_krokow, h, pole.x, pole.y)
         found_z = False
-        if lista_zamknieta[x][y] != None:
-            if lista_zamknieta[x][y].h > nowe.h:
-                lista_zamknieta[x][y] = nowe
+        if lista_zamknieta[y][x] != None:
+            if lista_zamknieta[y][x].h > nowe.h:
+                lista_zamknieta[y][x] = nowe
             found_z = True
         found_o = przeszukaj(lista_otwarta, nowe)
         if not found_o and not found_z:
@@ -101,14 +101,13 @@ def szukaj_min(lista):
 def szukaj_powrotu(cel, lista_zamknieta, mapa):
     x = cel.x
     y = cel.y
-    mapa[x][y] = "3"
-    while not x == S_X and not y == S_Y:
+    while not x == S_X or not y == S_Y:
+        mapa[y][x] = "3"
         tmp_x = x
         tmp_y = y
-        x = lista_zamknieta[tmp_x][tmp_y].rodzic_x
-        y = lista_zamknieta[tmp_x][tmp_y].rodzic_y
-        mapa[x][y] = "3"
-    mapa[S_X][S_Y] = "3"
+        x = lista_zamknieta[tmp_y][tmp_x].rodzic_x
+        y = lista_zamknieta[tmp_y][tmp_x].rodzic_y
+    mapa[S_Y][S_X] = "3"
 
 
 def wyswietl_mape(mapa):
@@ -125,7 +124,7 @@ def main():
     x = S_X
     y = S_Y
     ostatnie = Pole(x, y, 0, 0, x, y)
-    lista_zamknieta[x][y] = ostatnie
+    lista_zamknieta[y][x] = ostatnie
 
     while ostatnie.x != C_X or ostatnie.y != C_Y:
         sprawdz_pole(ostatnie, GORA, lista_zamknieta, lista_otwarta, mapa)
@@ -135,7 +134,7 @@ def main():
 
         if not lista_otwarta == []:
             ostatnie = szukaj_min(lista_otwarta)
-            lista_zamknieta[ostatnie.x][ostatnie.y] = ostatnie
+            lista_zamknieta[ostatnie.y][ostatnie.x] = ostatnie
             lista_otwarta.remove(ostatnie)
         else:
             print("Cel jest nieosiągalny!")
